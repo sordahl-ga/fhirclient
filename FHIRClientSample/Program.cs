c554db8047c5cfbe7cbcef872b6c9cb2913af0de
sordahl-ga <sordahl@bellsouth.net>
2019-03-20 10:06:22 -0400

    Add project files.

---
+++ FHIRClientSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FHIRClientSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
