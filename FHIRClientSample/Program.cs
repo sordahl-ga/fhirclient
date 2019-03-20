06171cdbc7280c52ff46373a164526a55eef4f33
sordahl-ga <sordahl@bellsouth.net>
2019-03-20 10:34:13 -0400

    Init

--- FHIRClientSample/Program.cs
+++ FHIRClientSample/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-
+using Hl7.Fhir.Model;
 namespace FHIRClientSample
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            FHIRSupport.FHIRClient client = new FHIRSupport.FHIRClient("<serverurl>", "<tenent>", "<clientid>", "<clientsecret>");
+            Patient p  = (Patient) client.LoadResource("Patient/<pat_id>");
+        }   
     }
 }
